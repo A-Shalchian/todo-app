c632b3752f018d4930d366fd7dc5c727622fff6c
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-02-18 22:21:11 -0500

    removed the header from the main page

--- todo-frontend/src/app/page.tsx
+++ todo-frontend/src/app/page.tsx
@@ -1,10 +1,8 @@
-import { Header } from "@/homeSections/Header";
 import { Hero } from "@/homeSections/Hero";
 export default function Home() {
   return (
-    <div>
-      <Header />
+    <>
       <Hero />
-    </div>
+    </>
   );
 }
