4dac694bd51f73b4ac977483ab345a99d83bef3d
A-Shalchian <162397874+A-Shalchian@users.noreply.github.com>
2025-02-18 22:21:45 -0500

    added metadata

--- todo-frontend/src/app/login/page.tsx
+++ todo-frontend/src/app/login/page.tsx
@@ -1,9 +1,15 @@
 import { LoginForm } from "@/loginSections/LoginForm";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Login - Todo App",
+  description: "Login to your account",
+};
 
 export default function LoginPage() {
   return (
-    <div>
+    <>
       <LoginForm />
-    </div>
+    </>
   );
 }
--- todo-frontend/src/app/signup/page.tsx
+++ todo-frontend/src/app/signup/page.tsx
@@ -1,9 +1,15 @@
 import { SignUpForm } from "@/loginSections/SignUpForm";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign Up - Todo App",
+  description: "Create your account",
+};
 
 export default function SignUpPage() {
   return (
-    <div className="">
+    <>
       <SignUpForm />
-    </div>
+    </>
   );
 }
